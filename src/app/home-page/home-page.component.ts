959cf913e9b7476ce3f20936891258b51aa196ee
omar cherdal <omarcherdal.1997@gmail.com>
2019-07-09 18:07:29 +0200

    zoro v2

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -12,12 +12,14 @@ export class HomePageComponent implements OnInit {
   someMethod(){
     this.trigger.openMenu();
   } */
-  array = [0,1];
+  array = [0,1,2,3,4];
   images=[
     'http://wowslider.com/sliders/demo-77/data1/images/idaho239691_1920.jpg',
     'http://wowslider.com/sliders/demo-77/data1/images/road220058.jpg',
     'https://biogenicnutrition.com/wp-content/uploads/2015/10/Tree-1600-x-600.jpg',
-    'https://www.axiomsl.com/wp-content/uploads/2018/05/img-Solutions-NEW.jpg'
+    'https://www.axiomsl.com/wp-content/uploads/2018/05/img-Solutions-NEW.jpg',
+    'https://www.trndlabs.com/wp-content/uploads/2016/06/trndlabs-site-banner-template-1600-600-decembers-shipping-4.jpg',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFcgnTQay0vEYlb7ij6U8ETYUnZNrneqBna0l7X6Fa3onsKcdj'
   ]
   constructor() { }
 
